21f21c1a18c7ac1517e614fd240327d5c1c913aa
insalamanca <insalamanca@gmail.com>
2018-07-25 20:55:44 -0500

    cargar datos en tabla posgres

---
+++ app/controllers/usuarios_controller.rb
@@ -0,0 +1,23 @@
+class UsuariosController < ApplicationController
+  def new
+  end
+
+
+  # Proceso para crear un usuario
+
+  def create
+    @usuario = Usuario.new usuario_params
+    @usuario.save
+  end
+
+  def index
+    @alluser = Image.all
+  end
+
+  private
+  def usuario_params
+    params.require(:usuario).permit(:nombre, :apellido, :correo)
+  end
+
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'usuarios/new'
+  #get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root :to => "welcome#index"
+
+  resources :usuarios
+  
+  root :to => "welcome#index"
+  
+end
