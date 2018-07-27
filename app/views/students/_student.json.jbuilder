36c5f3fc6dd21bf202f2ccdc39cd3aadf87da5ed
insalamanca <insalamanca@gmail.com>
2018-07-26 20:12:45 -0500

    scaffold rails

---
+++ app/views/students/_student.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! student, :id, :name, :last_name, :documnto, :created_at, :updated_at
+json.url student_url(student, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_002820) do
+ActiveRecord::Schema.define(version: 2018_07_27_011018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.integer "documnto"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "usuarios", force: :cascade do |t|
     t.string "nombre"
